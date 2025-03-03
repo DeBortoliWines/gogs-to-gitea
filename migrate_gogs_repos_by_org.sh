29934fb0a9f67fa91a896312f06f75ed74593499
Bernard Gray <bernard.gray@gmail.com>
2025-03-04 09:22:02 +1100

    make bash variable ref syntax consistent

--- migrate_gogs_repos_by_org.sh
+++ migrate_gogs_repos_by_org.sh
@@ -4,7 +4,7 @@
 
 set -x
 
-. $(dirname $0)/env || (echo "$(dirname $0)/env missing, please see $(dirname $0)/env.ex"; exit 1)
+. $(dirname $0)/env || (echo "$(dirname ${0})/env missing, please see $(dirname ${0})/env.ex"; exit 1)
 
 JSON_CACHE_REPO=/tmp/migrate-gitea-repo-${RANDOM}
 touch ${JSON_CACHE_REPO}
@@ -19,13 +19,13 @@ GET_ORGS=$(jq -r '.[] | .username' ${JSON_CACHE_ORG})
 #GET_ORGS=("pentaho" "archive")
 #for ORG_NAME in ${GET_ORGS[@]}; do
 
-for ORG_NAME in $GET_ORGS; do
+for ORG_NAME in ${GET_ORGS}; do
     ORG_DETAIL_CACHE=/tmp/migrate-gitea-${ORG_NAME}-${RANDOM}
     touch ${ORG_DETAIL_CACHE}
     curl -sH 'Accept:application/json' -X GET "https://${GOGS_HOST}/api/v1/orgs/${ORG_NAME}?token=${GOGS_TOKEN}"  | jq > ${ORG_DETAIL_CACHE}
 
     # create org in gitea
-    curl -sX POST "https://$GITEA_HOST/api/v1/orgs" -u $GITEA_USERNAME:$GITEA_TOKEN -H  "accept: application/json" -H  "Content-Type: application/json" -d "{  \
+    curl -sX POST "https://${GITEA_HOST}/api/v1/orgs" -u ${GITEA_USERNAME}:${GITEA_TOKEN} -H  "accept: application/json" -H  "Content-Type: application/json" -d "{  \
         \"email\": \"\", \
         \"username\": \"$(jq -r '.username' ${ORG_DETAIL_CACHE})\", \
         \"full_name\": \"$(jq -r '.full_name' ${ORG_DETAIL_CACHE})\", \
@@ -38,25 +38,25 @@ for ORG_NAME in $GET_ORGS; do
     GITEA_REPO_OWNER=${GOGS_ORGANISATION}
     
     # retrieve all repos in org
-    GET_REPOS=$(curl -sH 'Accept: application/json' -u $GOGS_USERNAME:$GOGS_TOKEN -s "https://${GOGS_HOST}/api/v1/orgs/${GOGS_ORGANISATION}/repos?token=$GOGS_TOKEN" | jq -r '.[] | .name')
+    GET_REPOS=$(curl -sH 'Accept: application/json' -u ${GOGS_USERNAME}:${GOGS_TOKEN} -s "https://${GOGS_HOST}/api/v1/orgs/${GOGS_ORGANISATION}/repos?token=${GOGS_TOKEN}" | jq -r '.[] | .name')
     
-    for REPO_NAME in $GET_REPOS; do
+    for REPO_NAME in ${GET_REPOS}; do
     
         curl -sH 'Accept: application/json' -s "https://${GOGS_HOST}/api/v1/repos/${GOGS_ORGANISATION}/${REPO_NAME}?token=${GOGS_TOKEN}" > ${JSON_CACHE_REPO}
         URL=$(jq -r '.clone_url' ${JSON_CACHE_REPO})
         DESC=$(jq -r '.description' ${JSON_CACHE_REPO})
     
-        echo "Found $REPO_NAME : ${URL}, importing..."
+        echo "Found ${REPO_NAME} : ${URL}, importing..."
     
-        curl -sX POST "https://$GITEA_HOST/api/v1/repos/migrate" -u $GITEA_USERNAME:$GITEA_TOKEN -H  "accept: application/json" -H  "Content-Type: application/json" -d "{  \
-        \"auth_username\": \"$GOGS_USERNAME\", \
-        \"auth_token\": \"$GOGS_TOKEN\", \
-        \"description\": \"$DESC\", \
-        \"clone_addr\": \"$URL\", \
+        curl -sX POST "https://${GITEA_HOST}/api/v1/repos/migrate" -u ${GITEA_USERNAME}:${GITEA_TOKEN} -H  "accept: application/json" -H  "Content-Type: application/json" -d "{  \
+        \"auth_username\": \"${GOGS_USERNAME}\", \
+        \"auth_token\": \"${GOGS_TOKEN}\", \
+        \"description\": \"${DESC}\", \
+        \"clone_addr\": \"${URL}\", \
         \"mirror\": false, \
         \"private\": true, \
-        \"repo_name\": \"$REPO_NAME\", \
-        \"repo_owner\": \"$GITEA_REPO_OWNER\", \
+        \"repo_name\": \"${REPO_NAME}\", \
+        \"repo_owner\": \"${GITEA_REPO_OWNER}\", \
         \"service\": \"gogs\", \
         \"uid\": 0, \
         \"issues\": false, \
